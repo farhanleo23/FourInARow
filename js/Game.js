5f348d61c3b055e6bc3d088005443bb01b96b17d
farhan hussain <farhanleo23@gmail.com>
2020-09-20 18:36:54 +0200

    add interactivity

--- js/Game.js
+++ js/Game.js
@@ -1,5 +1,4 @@
 class Game {
-    
     constructor() {
         this.board = new Board();
         this.players = this.createPlayers();
@@ -28,23 +27,50 @@ class Game {
     
     
     /** 
-     * Begins game. 
+     * Initializes game. 
      */
     startGame(){
         this.board.drawHTMLBoard();
         this.activePlayer.activeToken.drawHTMLToken();
         this.ready = true;
     }
-
-    handleKeyDown(e){
-        if (this.ready == true) {
-            if (e.key == 'ArrowRight') {
-                
-            } else if (e.key == 'ArrowLeft') {
-                
-            } else if (e.key == 'ArrowDown') {
-                
+	
+	
+    /**
+	 * Branches code, depending on what key player presses
+	 * @param	{Object}	e - Keydown event object
+	 */
+	handleKeyDown(e) {
+        if (this.ready) {
+            if (e.key === "ArrowLeft") {
+                this.activePlayer.activeToken.moveLeft();
+            } else if (e.key === "ArrowRight") {
+                this.activePlayer.activeToken.moveRight(this.board.columns);
+            } else if (e.key === "ArrowDown") {
+                this.playToken();
             }
         }
+    }
+    
+
+    /**
+     * Finds Space object to drop Token into, drops Token
+     */
+    playToken(){
+        let spaces = this.board.spaces;
+        let activeToken = this.activePlayer.activeToken;
+        let targetColumn = spaces[activeToken.columnLocation];
+        let targetSpace = null;
+
+		for (let space of targetColumn) {
+			if (space.token === null) {
+				targetSpace = space;
+			}
+        }
+
+        if (targetSpace !== null) {
+            game.ready = false;
+    		activeToken.drop(targetSpace);   
+        }              
     }
 }
--- js/Token.js
+++ js/Token.js
@@ -3,6 +3,7 @@ class Token {
         this.owner = owner;
         this.id = `token-${index}-${owner.id}`;
         this.dropped = false;
+        this.columnLocation = 0;
     }
     
     /** 
@@ -12,6 +13,16 @@ class Token {
     get htmlToken() {
         return document.getElementById(this.id);
     }
+	
+	
+    /** 
+     * Gets left offset of html element.
+     * @return  {number}   Left offset of token object's htmlToken.
+     */
+    get offsetLeft() {
+        return this.htmlToken.offsetLeft;
+    }
+	
     
     /** 
      * Draws new HTML token.
@@ -23,4 +34,41 @@ class Token {
         token.setAttribute('class', 'token');
         token.style.backgroundColor = this.owner.color;
     }
+	
+	
+    /** 
+     * Moves html token one column to left.
+     */
+    moveLeft() {
+        if (this.columnLocation > 0) {
+            this.htmlToken.style.left = this.offsetLeft - 76;
+            this.columnLocation -= 1;
+        } 
+    }
+    
+    
+    /** 
+     * Moves html token one column to right.
+     * @param   {number}    columns - number of columns on the game board    
+     */
+    moveRight(columns) {
+        if (this.columnLocation < columns - 1) {
+            this.htmlToken.style.left = this.offsetLeft + 76;
+            this.columnLocation += 1;
+        }
+    }
+    
+    
+    /** 
+     * Drops html token into targeted board space.
+     * @param   {Object}    Targeted space for dropped token.
+     * @param   {function}  The reset function to call after the drop animation has completed.
+     */
+	drop(target, reset) {
+        this.dropped = true;
+        
+        $(this.htmlToken).animate({
+            top: (target.y * target.diameter)
+        }, 750, 'easeOutBounce', reset);
+	}
 }
